311436c24c22fce1bd516453894f48f5b12dd622
Marcelo Salvador <macrusal@gmail.com>
2021-04-02 18:49:27 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+# REST-AWS
+Curso - Udemy
+
+https://www.udemy.com/course/restful-apis-do-0-a-nuvem-com-springboot-e-docker
+
+# Arquitetura da Aplicação
+* Java 11
+* Git
+* Spring Boot(com servidor Tomcat embarcado)
+
---
+++ src/main/java/br/com/hibejix/restaws/RestAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.hibejix.restaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAwsApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( RestAwsApplication.class, args );
+    }
+
+}
